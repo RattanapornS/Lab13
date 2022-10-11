60a0f4452b28dd02e115cb09869f531e3e3ce700
SawarinP <sawarin.p@kkumail.com>
2022-10-12 00:55:11 +0700

    mainTest6

--- Lab13/src/main/FizzBuzz.java
+++ Lab13/src/main/FizzBuzz.java
@@ -1,19 +1,16 @@
 package main;
 
-public class FizzBuzz {
-		public String convert(int number){
-			if (number == 5) {
-				return "Buzz";
-			}
-	        if(number == 4) {
-	            return "4";
-	        }
-	        if(number == 3) {
-	            return "Fizz";
-	        }
-	        if(number == 2){
-	            return "2";
-	        }
-	        return "1";
-	    }
+public class FizzBuzz {	
+		public String convert(final int number) {
+			
+			if(canDivideBy(number, 3) && canDivideBy(number, 5)) return "FizzBuzz";
+			if(canDivideBy(number, 5)) return "Buzz";
+			if(canDivideBy(number, 3)) return "Fizz";
+
+			return String.valueOf(number);
+		}
+
+		private Boolean canDivideBy(int number, int devider){
+			return number % devider == 0;
+		}
 	}
